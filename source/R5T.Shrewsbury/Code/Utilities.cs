7a1b519d53af5fd81fd6fed428b391858c495f92
MinexAutomation <minex.automation@gmail.com>
2019-12-13 11:53:23 -0800

    Changed parameter name.

--- source/R5T.Shrewsbury/Code/Utilities.cs
+++ source/R5T.Shrewsbury/Code/Utilities.cs
@@ -7,15 +7,15 @@ namespace R5T.Shrewsbury
 {
     public static class Utilities
     {
-        public static string GetConfigurationNameSpecificAppSettingsJsonFileName(string baseAppsettingsFileNameWithoutExtension, string configurationNameAppsettingsFileNameToken)
+        public static string GetConfigurationNameSpecificAppSettingsJsonFileName(string baseAppsettingsFileNameWithoutExtension, string configurationNameAppSettingsFileNameToken)
         {
-            var appSettingsJsonFileName = $"{baseAppsettingsFileNameWithoutExtension}.{configurationNameAppsettingsFileNameToken}.{FileExtensions.Json}";
+            var appSettingsJsonFileName = $"{baseAppsettingsFileNameWithoutExtension}.{configurationNameAppSettingsFileNameToken}.{FileExtensions.Json}";
             return appSettingsJsonFileName;
         }
 
-        public static string GetConfigurationNameSpecificAppSettingsJsonFileName(string configurationNameAppsettingsFileNameToken)
+        public static string GetConfigurationNameSpecificAppSettingsJsonFileName(string configurationNameAppSettingsFileNameToken)
         {
-            var appsSettingsJsonFileName = Utilities.GetConfigurationNameSpecificAppSettingsJsonFileName(Constants.DefaultBaseAppSettingsFileNameWithoutExtension, configurationNameAppsettingsFileNameToken);
+            var appsSettingsJsonFileName = Utilities.GetConfigurationNameSpecificAppSettingsJsonFileName(Constants.DefaultBaseAppSettingsFileNameWithoutExtension, configurationNameAppSettingsFileNameToken);
             return appsSettingsJsonFileName;
         }
     }
